66e8902ac4fec41892b44f28ea72b68f2bd8e149
YOUR_GITHUB_USERNAME <dave.sloan@gmail.com>
2016-04-19 14:44:20 -0700

    adding hover treatments

--- base.js
+++ base.js
@@ -1,17 +1,38 @@
 console.log("JS is working!");
-
 $(document).ready(function() {
-
 	$("#button1").click(function(){
 		$("#red-box").hide();
 	});
-
 	$("#button2").click(function(){
-		$("#orange-box").fadeOut(2000);
+	$("#orange-box").fadeOut(2000);
 	});
-	
+
 	$("#button3").click(function(){
 		$("#yellow-box").text("thanks for clicking!");
 	});
-	
+
+	$("#one").hover(function(){
+		$(this).css("background-color", "red");
+	}, function(){
+		$(this).css("background-color", "white");
+	});
+
+	$("#two").hover(function(){
+		$(this).css("background-color", "orange");
+	}, function(){
+		$(this).css("background-color", "white");
+	});
+
+	$("#three").hover(function(){
+		$(this).css("background-color", "yellow");
+	}, function(){
+		$(this).css("background-color", "white");
+	});
+
+	$("#four").hover(function(){
+		$(this).css("background-color", "green");
+	}, function(){
+		$(this).css("background-color", "white");
+	});
+
 });
